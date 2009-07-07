d0c9b5e6dfc3cad1c0192a786e5c15c9a52229f4
Papa Pathe Sene <pathesene@netsyscom.biz>
2009-07-07 06:51:24 +0200

    continue code refactoring

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,22 @@
 module ArticlesHelper
+	def push_articles(article)	
+		@content = titlelize(article)
+		@content << trunc_article(article)	
+		@content << readmore(article)	
+	end
+	
+	def trunc_article(article)
+	    id = article.content.index('<!-- pagebreak -->') + 2
+	    truncate(article.content , id)
+	end
+	
+	def titlelize(article)
+		content_tag :h2 do 
+		  link_to article.title , article_path(article)
+		end
+	end
+	
+	def readmore(article)
+		link_to 'Lire la suite' , article_path(article)
+	end
 end
