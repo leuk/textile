dbadddc717da69f1648f8e7608301abb30895d0e
Papa Pathe Sene <pathesene@netsyscom.biz>
2009-07-07 04:01:42 +0200

    slideshow code refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :articles
-
   map.resources :sections
+  map.root :controller => 'articles'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
